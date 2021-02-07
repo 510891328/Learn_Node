5816419f23621e99c88a0a1e209554d29a7818b0
510891328 <as510891328@gmail.com>
2021-02-06 21:57:44 -0500

    able to create server with a submitable form and redirect

---
+++ FirstPractice/server.js
@@ -0,0 +1,25 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    const method = req.method
+
+    if( url === '/'){
+        res.write("<html>")
+        res.write("<title>First Practice</title>")
+        res.write("<body>")
+        res.write("<form action='/user' method='POST'>")
+        res.write("<input type='text' name='username'>")
+        res.write("<button type='submit'>Submit</button>")
+        res.write("</form>")
+        res.write("</body>")
+        res.write("</html>")
+        res.end();
+    }
+
+    if( url === '/user' && method === 'POST'){
+        console.log(res);
+    }
+})
+
+server.listen(8080);
