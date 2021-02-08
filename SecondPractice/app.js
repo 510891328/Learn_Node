cc69884be68d749e63f11fe1006227841278b8be
510891328 <as510891328@gmail.com>
2021-02-08 18:44:58 -0500

    express with general server and routes

---
+++ SecondPractice/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.use('/user', (req, res, next) => {
+    res.send('<h1>Hi this is User</h1>')
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Home Directory</h1>')
+})
+
+app.listen('8080')
