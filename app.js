0f66ec05f4b7c311572f1553081688d213658645
510891328 <as510891328@gmail.com>
2021-02-10 14:56:16 -0500

    routing practice

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const userRoutes = require('./routes/home');
+
+app.use(userRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found</h1>')
+})
+
+app.listen('8080')
